409b9346b4d7dd9e2888ca4b38058369279575a0
Dean Mullinger <mullingerdean@gmail.com>
2020-12-01 05:24:02 +0000

    keys upload

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./components/layout/Navbar"
 import ReactTable from "./components/ui/ReactTable"
 import AppState from "./context/api/AppState"
 import "../src/styles/App.css"
+require('dotenv').config()
 
 const App = () => {
   const [meteors, setMeteors] = useState([])
